e7aa43eb3d5be559235579e56b89f0d654658413
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-01 12:54:31 -0700

    Debugging
    A bunch of debugging on this. I mostly had some dumb off by one errors
    on the version I wrote (the first one) and some small logic stuff, and
    the rest of the errors were all misuse of lambda expressions where I
    should use def statements, and such.

--- 2-Q3.py
+++ 2-Q3.py
@@ -1,32 +1,62 @@
-square = lambda x: x*x
-'''
-def f_to_n(f, n):
-  funcy, i = f, 0
-  while i <= n:
-    funcy = lambda x: f(funcy(x))
+def accumulate(combiner, start, n, term):
+  acc, i = start, 1
+  while i<= n:
+    acc = combiner(term(i), acc)
     i += 1
-  return funcy
+  return acc
+
+square = lambda x: x*x
+
+def composer(f, g):
+  def h(x):
+    return f(g(x))
+  return h
 
 def f_to_n(f, n):
+  funcy = f
+  while n > 1:
+    funcy, n = composer(f, funcy), n - 1
+  return funcy
+
+
+def f_to_n_(f, n):
   def composite(x):
-    acc, i = x,1
-    while i<n:
+    acc = x
+    i = 0
+    while i < n:
       acc = f(acc)
       i += 1
     return acc
   return composite
-'''
-composes(f, g) = lambda x: f(g(x))
-def f_to_n(f, n):
-  return accumulate(compose(f, f), 0, n, lambda k: f)
 
-f_to_n(square, 2)(5)
+def f_to_n__(f, n):
+  return accumulate(composer, f, n-1, (lambda k: f))
+
+print(f_to_n(square, 2)(5))
 #should be 625
-f_to_n(square, 4)(5)
+print(f_to_n(square, 4)(5))
 #should be 152587890625
-f_to_n(lambda x: x + 1, 5)(309)
+print(f_to_n(lambda x: x + 1, 5)(309))
 #should be 314
-f_to_n(lambda x: 2**x, 3)(2)
+print(f_to_n(lambda x: 2**x, 3)(2))
+#should be 65536
+
+print(f_to_n_(square, 2)(5))
+#should be 625                                                                
+print(f_to_n_(square, 4)(5))
+#should be 152587890625                                                       
+print(f_to_n_(lambda x: x + 1, 5)(309))
+#should be 314                                                                
+print(f_to_n_(lambda x: 2**x, 3)(2))
+#should be 65536
+
+print(f_to_n__(square, 2)(5))
+#should be 625                                                                
+print(f_to_n__(square, 4)(5))
+#should be 152587890625                                                       
+print(f_to_n__(lambda x: x + 1, 5)(309))
+#should be 314                                                                
+print(f_to_n__(lambda x: 2**x, 3)(2))
 #should be 65536
 
 def double(f):
