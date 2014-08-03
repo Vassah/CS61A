bfab65c0400010b5bd2d950e84a4bfdc124e2fa6
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-02 18:33:27 -0700

    Rename 3-Q4, HW4 Work
    Parts 1,2,3 done on HW4.

---
+++ HW4.py
@@ -0,0 +1,77 @@
+#We are asked to implement a 'word' abstract data type.
+#Basically, it's an OCaml type of form [Char, [Char]]
+
+def make_word_from_string(s):
+  #Creates an instance of the word ADT from the string s.
+  lst = []
+  for i in range(0, len(s)):
+    lst.append(s[i])
+  return [s, lst]
+
+def make_word_from_list(lst):
+  #Creates an instance of the word ADT from the list of characters lst.
+  s = ''
+  for i in lst:
+    s += i
+  return [s, lst]
+
+def get_string(word):
+  #Returns the string representation of word.
+  return word[0]
+
+print(get_string(make_word_from_string('hello')))
+#Should be 'hello'
+print(get_string(make_word_from_list(['w', 'o', 'r', 'l', 'd'])))
+#Should be 'world'
+
+def get_list(word):
+  #Returns the list of characters representation of word.
+  return word[1]
+
+print(get_list(make_word_from_string('hello')))
+#Should be ['h', 'e', 'l', 'l', 'o']
+print(get_list(make_word_from_list(['w', 'o', 'r', 'l', 'd'])))
+#Should be ['w', 'o', 'r', 'l', 'd']
+
+#Now we are asked to implement a function that finds number of common letters
+#We simplify the problem:
+
+def word_wo_reps(word):
+  lz = get_list(word)
+  acc = []
+  for i in lz:
+    if not i in acc:
+      acc.append(i)
+  return mwfl(acc)
+
+#We test to see that works.
+mwfs, mwfl = make_word_from_string, make_word_from_list
+print(mwfs('hello'))
+print(get_list(mwfs('hello')))
+print(word_wo_reps(mwfs('hello')))
+print(get_string(word_wo_reps(mwfs('hello'))))
+#should be 'helo'
+print(get_string(word_wo_reps(mwfs('helo'))))
+#should be 'helo'
+
+#So that's finally on track!
+#Onward ho. Since only guess has repetitions, let's just eliminate them
+
+def num_common_letters(goal, guess):
+  guess = word_wo_reps(guess)
+  acc = 0
+  for i in get_list(guess):
+    for j in get_list(goal):
+      if i ==j:
+        acc += 1
+  return acc
+
+print(num_common_letters(mwfs('hello'), mwfs('hell')))
+#Should be 4
+print(num_common_letters(mwfs('hello'), mwfs('red')))
+#Should be 1
+print(num_common_letters(mwfs('hello'), mwfs('rid')))
+#Should be 0
+
+def make_word_master(goal_word):
+  word_len = len(get_string(goal_word))
