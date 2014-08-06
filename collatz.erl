b0bfc7a1f5e9c3e9511e7562aa82b15857ddeb4e
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-06 09:42:21 -0700

    Update collatz.erl
    There is no modulus operator in Erlang! Which makes sense given what Erlang is for. It's not hard to implement anyway given the language's native cababilities. I don't know that using guards is the best thing, but it's straightforward enough that it's not worth really worrying about.

--- collatz.erl
+++ collatz.erl
@@ -1,7 +1,11 @@
 -module(collatz).
 -module([hailstone/1]).
 
+%Assumes that both n and m are positive. This is sufficient for the needs of the hailstone function, so why waste time?
+mod(n,m) where n < m -> n;
+mod(n,m) where n >= m -> mod(n - m, m).
+
 hailstone(1) -> [1|[]];
-hailstone(n) where n % 2 == 0 -> [n|hailstone(n / 2)];
+hailstone(n) where mod(n, 2) == 0 -> [n|hailstone(n / 2)];
 hailstone(n) -> [n|hailstone(3 * n + 1)].
   
