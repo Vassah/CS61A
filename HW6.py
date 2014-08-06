f7a4d91676992c0d0101c150170d163304606376
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-05 20:46:47 -0700

    HW6-Q3
    Progress is made but it doesn’t quite work right yet.

--- HW6.py
+++ HW6.py
@@ -52,4 +52,79 @@ print(numberize('+'))
 #Should be '+'
 
 def read_exp(token_lz):
-  
+  if token_lz == []:
+    raise SyntaxError('unexpected end of input')
+  token, rest = token_lz[0], token_lz[1:]
+  if token == ')':
+    raise SyntaxError('unexpected )')
+  elif token == '(':
+    if rest == []:
+      raise SyntaxError('mismatched parentheses')
+    elif rest[0] == ')':
+      raise SyntaxError('empty combination')
+    else:
+      return read_until_close(rest)
+  else:
+    return read_exp(rest)
+
+def cons(a, b):
+  def cell(message):
+    if message == 'car':
+      return a
+    else:
+      return b
+  return cell
+
+def car(cell):
+  return cell('car')
+
+def cdr(cell):
+  return cell('cdr')
+
+empty = cons(None, None)
+
+def make_link_list(lz):
+  if lz == []:
+      return empty
+  return cons(lz[0], make_link_list(lz[1:]))
+
+#I assume the last cell of any linked list is cons(a, empty).                               
+def len(linklist):
+  if cdr(linklist) == empty or linklist == empty:
+    return 1 #base case is to add 1 since there's still the car of this last cell to be counted.                                                                                       
+  else:
+    return 1 + len(cdr(linklist))
+
+def print_linked_list(lz):
+  s ='<'
+  while cdr(lz) != empty:
+    s = s + str(car(lz)) + ' '
+    lz = cdr(lz)
+  s = s + str(car(lz)) + ' '
+  print(s + '>')
+
+def read_until_close(open_expr):
+  def aux(oped,acc):
+    if oped == None or oped == []:
+      raise SyntaxError('No Closure')
+    token,rest = oped[0], oped[1:]
+    if token == '(':
+      acc = cons(read_exp(oped), acc)
+      return aux(rest, acc)
+    elif token == ')':
+      return acc, rest
+    else:
+      acc = cons(numberize(token), acc)
+      return aux(rest, acc)
+  return aux(open_expr, empty)
+
+exp, unevaled = read_until_close(['+', '2', '3', ')', '4', ')'])
+print_linked_list(exp)
+#Should be < '+' 2 3 >
+print(unevaled)
+#Should be ['4', ')']
+exp, unevaled = read_until_close(['+', '(', '-', '2', ')', '3', ')', ')'])
+print_linked_list(exp)
+#Should be < '+' < '-' 2 > 3 > NOPE DOESN'T WORK
+print(unevaled)
+#Should be [')'] NOPE DOESN'T WORK
