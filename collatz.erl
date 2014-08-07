552f8b16da07a03d3a555d5d07c1b2da097efb3d
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-06 23:38:56 -0700

    Hailstone Erl Modifications

--- collatz.erl
+++ collatz.erl
@@ -1,11 +1,11 @@
 -module(collatz).
 -module([hailstone/1]).
 
-%Assumes that both n and m are positive. This is sufficient for the needs of the hailstone function, so why waste time?
+%Assumes that both n and m are positive. 
+%This is sufficient for the needs of the hailstone function, so why waste time?
 mod(n,m) where n < m -> n;
 mod(n,m) where n >= m -> mod(n - m, m).
 
 hailstone(1) -> [1|[]];
 hailstone(n) where mod(n, 2) == 0 -> [n|hailstone(n / 2)];
 hailstone(n) -> [n|hailstone(3 * n + 1)].
-  
