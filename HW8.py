56089ec960293645af45d32dea9e2d827badad9b
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-07 23:58:01 -0700

    Q4 Finis
    This one was really easy.

--- HW8.py
+++ HW8.py
@@ -75,3 +75,52 @@ print(m)
 a = MinimalAmount(0,50)
 m = a.nickels, a.pennies, a.value
 print(m)
+
+#Q4
+class VendingMachine(object):
+  def __init__(self, thing, cost):
+     self.cost = cost
+     self.thing = thing
+     self.balance = 0
+     self.stock = 0
+
+  def restock(self, amount):
+     self.stock = self.stock + amount
+     print('Current {0} stock: {1}'.format(self.thing, self.stock))
+
+  def deposit(self, payment):
+     self.balance = self.balance + payment
+     print('Current Balance: ${0}'.format(self.balance))
+
+  def vend(self):
+    if self.balance == self.cost:
+        print('Here is your {0}.'.format(self.thing))
+        self.balance = 0
+    elif self.balance > self.cost:
+        print('Here is your {0} and ${1} change.'.format(self.thing, self.balance - self.cost))
+        self.balance = 0
+    else:
+        print('You must deposit ${0} more.'.format(self.cost - self.balance))
+
+print("\nVendingMachine?")
+v = VendingMachine('candy', 10)
+v.vend()
+v.restock(1)
+v.vend()
+v.deposit(7)
+v.vend()
+v.restock(1)
+v.deposit(5)
+v.vend()
+v.deposit(10)
+v.vend()
+v.deposit(15)
+v.vend()
+p = VendingMachine('pepsi', 21)
+p.restock(100)
+p.deposit(100)
+p.vend()
+p.deposit(21)
+p.vend()
+
+#Q5
