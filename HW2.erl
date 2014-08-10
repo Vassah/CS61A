effcc04a7bf87505e1d03335020815e6f9314b2b
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-10 00:53:04 -0700

    Werid.
    This looks like it should work to me. Something is very odd about the
    way Erlang does higher order functions…

--- HW2.erl
+++ HW2.erl
@@ -1,1 +1,10 @@
-% To be done tonight/tomorrow
+-module(hw2).
+-export([productus/2, factorialus/1, range/1]).
+
+range(0) -> [];
+range(n) -> [n | range(n - 1)].
+
+productus(0, _) -> 1;
+productus(n, F) -> list:map(F, range(n)).
+
+factorialus(m) -> productus(m, fun(x) -> x end).
