f2afaea69d7ec937bb979bc459564ba1a2d30f80
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-11 15:06:21 -0700

    HW2 Haskell Finis

--- HW2.hs
+++ HW2.hs
@@ -11,4 +11,19 @@ accumulate combiner start n term
   |start == n = term n
   |otherwise  = combiner (term start) (accumulate combiner (start + 1) n term)
 
+multus :: Int -> Int -> Int
+multus x y = x*y
+
+addit :: Int -> Int -> Int
+addit x y = x + y
+
+productus n term = accumulate multus 1 n term
+
+summatus n term = accumulate addit 0 n term
+
+repeated _ 0 = identat
+repeated fz n = (\ x -> fz (repeated fz (n - 1)))
+
+double fz = repeated fz 2
+
 main = putStrLn (show (seq_product 5 identat) ++ show (factorialus 5) ++ show (accumulate (\x y -> x * y) 1 5 (\x -> x)))
