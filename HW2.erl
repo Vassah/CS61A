3e06895073b3acd7d4863a79d4e74fe4207216fb
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-10 19:35:40 -0700

    Modifications

--- HW2.erl
+++ HW2.erl
@@ -2,9 +2,9 @@
 -export([productus/2, factorialus/1, range/1]).
 
 range(0) -> [];
-range(n) -> [n | range(n - 1)].
+range(N) -> [N | range(N - 1)].
 
 productus(0, _) -> 1;
-productus(n, F) -> list:map(F, range(n)).
+productus(N, F) -> F(N)*productus(N - 1, F).
 
-factorialus(m) -> productus(m, fun(x) -> x end).
+factorialus(M) -> productus(M, fun(x) -> x end).
